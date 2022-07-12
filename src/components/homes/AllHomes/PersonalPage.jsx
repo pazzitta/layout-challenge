37218091623a04d63df87739b27ae19986eac5b0
Paz Zitta <zittapaz@gmail.com>
2022-07-12 08:55:33 -0300

    finished html layout PersonalPage

--- src/components/homes/AllHomes/PersonalPage.jsx
+++ src/components/homes/AllHomes/PersonalPage.jsx
@@ -33,6 +33,30 @@ const PersonalPageHome = () => {
                     <a href="tel:3536272883"></a>
                 </div>
             </nav>
+            <div>
+                <div>
+                    <h5>Hi There,</h5>
+                    <h1>I am Venissa</h1>
+                    <h2>I am designer during daytime</h2>
+                    <h3>& work with batman during nigths.</h3>
+                    <button>Ask Me How</button>
+                </div>
+                <img src="" alt="Imagen de ella" />
+            </div>
+            <div>
+                <div>
+                    <h6>Email</h6>
+                    <p>soyunemail@gmail.com</p>
+                </div>
+                <div>
+                    <h6>Phone</h6>
+                    <p>----------</p>
+                </div>
+                <div>
+                    <h6>Location</h6>
+                    <p>Soy una dirección</p>
+                </div>
+            </div>
         </div>
     )
 }
