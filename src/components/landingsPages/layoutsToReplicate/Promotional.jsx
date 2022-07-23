1f1e0b1d9ae5faea2f5189eed26c37f8f41bf549
Paz Zitta <zittapaz@gmail.com>
2022-07-22 21:04:26 -0300

    almost finished css laoyout Promotion, missing details and the next factory

--- src/components/landingsPages/layoutsToReplicate/Promotional.jsx
+++ src/components/landingsPages/layoutsToReplicate/Promotional.jsx
@@ -1,4 +1,6 @@
 import ImagePromotional from '../../../assets/LandingPages/imageP.jpg'
+import IsoPromotional from '../../../assets/LandingPages/isoPromotion.jpg'
+import LupaPromotional from '../../../assets/LandingPages/isoPromotion.jpg'
 
 const Promotional= () => {
     return(
@@ -6,17 +8,17 @@ const Promotional= () => {
             <img className='image-promotional-LP' src={ImagePromotional} alt="Imagen diseño interior"/>
             <header className='box-header-promotional-LP'>
                 <div className='box-ancords-header-promotional-LP'>
-                    <img src="" alt="Logo" />
-                    <a href="">Home</a>
-                    <a href="">Interiors</a>
-                    <a href="">About</a>
+                    <img className='isologo-header-promotional-LP' src={IsoPromotional} alt="Logo" />
+                    <a className='ancords-header-organic-shapes-LP' href="">Home</a>
+                    <a className='ancords-header-organic-shapes-LP' href="">Interiors</a>
+                    <a className='ancords-header-organic-shapes-LP' href="">About</a>
                 </div>
                 <div className='box-search-header-promotional-LP'>
                     <div>
-                        <button>Lupa</button>
+                        <button className='button-magnifying-glass-promotional-LP'></button>
                         {/* <input type="text" /> */}
                     </div>                
-                    <select name="" id="">
+                    <select className='select-header-promotional-LP' name="" id="">
                         <option selected></option>
                         <option>Interior 1</option>
                         <option>Interior 2</option>
@@ -24,22 +26,22 @@ const Promotional= () => {
                 </div>
             </header>
             <main className='box-main-promotional-LP'>
-                <div>
-                    <h1>Interior design for you</h1>
-                    <small>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum odit ipsam eveniet saepe, quibusdam neque, quia iusto fuga.</small>
-                    <button>EXPLORE</button>
-                </div>
+                    <h1 className='titel-main-promotional-LP'>Interior design for you</h1>
+                    <p className='description-main-promotional-LP'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum odit ipsam eveniet saepe, quibusdam neque, quia iusto fuga.</p>
+                    <div className='box-button-main-promotional-LP'>
+                        <button className='button-main-promotional-LP'>EXPLORE</button>
+                    </div>
             </main>
             <footer className='box-footer-promotional-LP'>
-                <div>
-                    <h5>Single price</h5>
-                    <p>Lorem ipsum dolor sit.Repellendus, officia?</p>
+                <div className='box-one-footer-promotional-LP'>
+                    <h5 className='titel-footer-promotional-LP'>Single price</h5>
+                    <p className='description-footer-promotional-LP'>Lorem ipsum dolor sit.Repellendus, officia?</p>
                 </div>
-                <div>
-                    <h5>Adaptation</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <div className='box-two-footer-promotional-LP' >
+                    <h5 className='titel-footer-promotional-LP'>Adaptation</h5>
+                    <p className='description-footer-promotional-LP'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                 </div>
-                <div>
+                <div className='box-three-footer-promotional-LP'>
                     <h6>NEXT FACTORY</h6>
                 </div>
             </footer>
