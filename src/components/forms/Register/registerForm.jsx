69796507e2ea12475e573ed8e37b1cab5468f335
Paz Zitta <zittapaz@gmail.com>
2022-08-06 17:36:26 -0300

    finished css layout box left and box right in process

--- src/components/forms/Register/registerForm.jsx
+++ src/components/forms/Register/registerForm.jsx
@@ -1,4 +1,9 @@
 import './registerForm.css'
+import IsologoWhite from '../../../assets/Forms/Register/isologoWhite.png'
+import IconFacebookBlack from '../../../assets/Forms/Register/iconFacebookBlack.png'
+import IconGoogleBlack from '../../../assets/Forms/Register/iconGoogleBlack.png'
+import IconLinkedInBlack from '../../../assets/Forms/Register/iconLinkedInBlack.png'
+
 
 const RegisterForm = () => {
     return(
@@ -6,17 +11,17 @@ const RegisterForm = () => {
             {/* <img src="" alt="Imagen de fondo" /> va a ser u background en el div mayor */} 
             <div className='box-register-form'>
                 <section className='box-left-register-form'>
-                    <img src="" alt="Logo" />
+                    <img src={IsologoWhite} alt="Isologo" />
                     <h2>Welcome Back!</h2>
-                    <p>To keep connected with please login with your personal info</p>
-                    <button>Sing in</button>
+                    <p>To keep connected with us please login with your personal info</p>
+                    <button>SING IN</button>
                 </section>
                 <section className='box-right-register-form'>
                     <h1>Create Account</h1>
                     <div>
-                        <button></button>
-                        <button></button>
-                        <button></button>
+                       <a href=""><img src={IconFacebookBlack} alt="ícono Facebook" /></a>
+                       <a href=""><img src={IconGoogleBlack} alt="ícono Google" /></a>
+                       <a href=""><img src={IconLinkedInBlack} alt="ícono LinkedIn" /></a>
                     </div>
                     <p>or use your email for registration</p>
                     <input type="text" />
