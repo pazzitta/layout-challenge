8c3d88b50a981bd0398f3bd5ddf172158b08de9a
Paz Zitta <zittapaz@gmail.com>
2022-08-18 12:15:34 -0300

    i almost have the folder, it's not long

--- src/components/myHome/InternalBoardComponents/layoutFolder.jsx
+++ src/components/myHome/InternalBoardComponents/layoutFolder.jsx
@@ -3,18 +3,38 @@ import './layoutFolder.css'
 const LayoutFolder = () => {
     return(
         <div className='box-layout-folder'>
-            <div className='back-folde-order'>
-                <div className='flap-folder-order-and-style'></div>                
-                <div className='base-folder-style'></div>
+            <div className='box-back-folder'>
+                <div className='back-folde-order'>
+                    <div className='flap-folder-order-and-style'></div>                
+                    <div className='base-folder-style'></div>
+                </div>
+                <div className='front-folde-order'>
+                    <div className='flap-front-folder-order-and-style'>
+                        <div>
+                            <div></div>
+                            <h4>Namename</h4>
+                        </div>
+                    </div>                
+                    <div className='base-front-folder-style'></div>
+                </div>
             </div>
-            <div className='front-folde-order'>
-                <div className='flap-front-folder-order-and-style'>
+            
+            <div className='box-fron-folder'>
+                <div className='front-front-cover-folde-order'>
+                    <div className='flap-front-front-folder-order-and-style'></div>                
+                    <div className='base-front-front-folder-style'></div>
+                </div>
+                <div className='box-paper-markets'>
+                    <div></div>
+                    <div></div>
                     <div>
                         <div></div>
-                        <h4>Namename</h4>
+                        <div></div>
+                        <div></div>
+                        <div></div>
                     </div>
-                </div>                
-                <div className='base-front-folder-style'></div>
+                </div>
+        
             </div>
 
         </div>
