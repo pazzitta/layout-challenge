067a8838d6081478c9d27088d6d0bb4022e35911
Paz Zitta <zittapaz@gmail.com>
2022-08-18 22:23:40 -0300

    LPBoardContent almost ready

--- src/components/landingsPages/MainPageBase/LPBoardContent.jsx
+++ src/components/landingsPages/MainPageBase/LPBoardContent.jsx
@@ -1,16 +1,20 @@
 import './mainPagesBase.css'
 import RowOfPencils from '../../myHome/InternalBoardComponents/rowOfPencils';
+import BackgroundVideoLP from '../../../assets/LandingPages/backgroundVideo.jpg'
+import MinimalismLP from '../../../assets/LandingPages/Minimalism.jpg'
+import OrganicShapesLP from '../../../assets/LandingPages/OrganicShapes.jpg'
+import AdvertisingLP from '../../../assets/LandingPages/Advertising.jpg'
 
 const LPBoardContent = () => {
     return(
         <div className='order-all-board-content-LP'>
             <div className='original-pages-order-and-styles'>
-                <div>
+                <div className='wooden-sign-background'>
                     <div></div>
                     <h2>Original pages</h2>
                     <div></div>
                 </div>
-                <div>
+                <div className='order-wooden-signs'>
                     <div>
                         <div></div>
                         <h3>Background video</h3>
@@ -31,6 +35,12 @@ const LPBoardContent = () => {
                         <h3>Advertising</h3>
                         <div></div>
                     </div>                
+                </div>
+                <div className='original-page-images-order'>
+                    <img src={BackgroundVideoLP} alt="" />
+                    <img src={MinimalismLP} alt="" />
+                    <img src={OrganicShapesLP} alt="" />
+                    <img src={AdvertisingLP} alt="" />
                 </div>
             </div>
             <div className='my-replicas-order-and-styles'></div>
