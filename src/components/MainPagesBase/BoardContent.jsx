1db42c80e4c9fc24462effcef2786cf2461083af
Paz Zitta <zittapaz@gmail.com>
2022-08-20 15:21:54 -0300

    I finished the main pages and the general modal layout for them, they were beautiful!

--- src/components/MainPagesBase/BoardContent.jsx
+++ src/components/MainPagesBase/BoardContent.jsx
@@ -1,10 +1,11 @@
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import './mainPagesBase.css'
 import RowOfPencils from '../myHome/InternalBoardComponents/rowOfPencils';
 import BackgroundVideoLP from '../../assets/LandingPages/BackgroundVideoGif.gif'
 import MinimalismLP from '../../assets/LandingPages/Minimalism.jpg'
 import OrganicShapesLP from '../../assets/LandingPages/OrganicShapes.jpg'
 import PromotionalLP from '../../assets/LandingPages/Promotional.jpg'
-import { useLocation } from 'react-router-dom';
 import ECommerceH from '../../assets/homes/Ecommerce.jpg'
 import MarketPlaceH from '../../assets/homes/Marketplace.jpg'
 import PersonalPageH from '../../assets/homes/PersonalPage.jpg'
@@ -26,6 +27,13 @@ import Modal from '../Modal/modal';
 
 const AllBoardContent = () => {
     const location = useLocation()
+
+    const [stateModal1, SetstateModal1] = useState(false)
+    const [stateModal2, SetstateModal2] = useState(false)
+    const [stateModal3, SetstateModal3] = useState(false)
+    const [stateModal4, SetstateModal4] = useState(false)
+
+
 
     return(
         <>
@@ -77,7 +85,7 @@ const AllBoardContent = () => {
                              location.pathname === "/homes" ? <h3>Marketplace</h3>:
                              location.pathname === "/details" ? <h3>News</h3>:
                              location.pathname === "/forms" ? <h3>CheckOut</h3>:
-                             <h3>Profile</h3>
+                             <h3>News</h3>
                         }
                         <div></div>
                     </div>
@@ -88,7 +96,7 @@ const AllBoardContent = () => {
                              location.pathname === "/homes" ? <h3>News</h3>:
                              location.pathname === "/details" ? <h3>Profile</h3>:
                              location.pathname === "/forms" ? <h3>Contact Us</h3>:
-                             <h3>News</h3>
+                             <h3>Profile</h3>
                         }                       
                         <div></div>
                     </div>                
@@ -150,20 +158,44 @@ const AllBoardContent = () => {
                     <div></div>
                 </div>
                 <div className='my-replicas-images-order'>
-                    <div>
-                        <button></button>
+                    <div onClick={() => SetstateModal1(!stateModal1)}>
+                    {
+                        location.pathname === "/landingPages" ?  <img src={BackgroundVideoLP} alt="imagen original Background Video LP"/>:
+                        location.pathname === "/homes" ? <img src={PersonalPageH} alt="imagen original personal page H"/>:
+                        location.pathname === "/details" ? <img src={ClothesD} alt="imagen original Clothes D"/>:
+                        location.pathname === "/forms" ? <img src={LoginF} alt="imagen original Login LP"/>:
+                        <img src={ClothesC} alt="imagen original Clothes C"/>
+                    }
                         <h3>Discover me</h3>
                     </div>
-                    <div>
-                        <button></button>
+                    <div onClick={() => SetstateModal2(!stateModal2)}>
+                    {
+                        location.pathname === "/landingPages" ?  <img src={MinimalismLP} alt="imagen original Minimalism LP" />:
+                        location.pathname === "/homes" ? <img src={ECommerceH} alt="imagen E-commerce H"/>:
+                        location.pathname === "/details" ? <img src={FurnitureD} alt="imagen original Furniture D"/>:
+                        location.pathname === "/forms" ? <img src={RegisterF} alt="imagen original Register F"/>:
+                        <img src={FoodC} alt="imagen original Food C"/>
+                    }
                         <h3>Discover me</h3>
                     </div>
-                    <div>
-                        <button></button>
+                    <div onClick={() => SetstateModal3(!stateModal3)}>
+                    {
+                        location.pathname === "/landingPages" ?   <img src={OrganicShapesLP} alt="imagen original OrganicShapes LP" />:
+                        location.pathname === "/homes" ? <img src={MarketPlaceH} alt="imagen Marketplace H"/>:
+                        location.pathname === "/details" ? <img src={NewsD} alt="imagen original News D"/>:
+                        location.pathname === "/forms" ? <img src={CheckOutF} alt="imagen original CheckOut F"/>:
+                        <img src={NewsC} alt="imagen original News C"/>
+                    }
                         <h3>Discover me</h3>
                     </div>
-                    <div>
-                        <button></button>
+                    <div onClick={() => SetstateModal4(!stateModal4)}>
+                    {
+                        location.pathname === "/landingPages" ? <img src={PromotionalLP} alt="imagen original Promotional LP" />:
+                        location.pathname === "/homes" ? <img src={DigitalNewsPaperH} alt="imagen DigitalNewsPaper H"/>:
+                        location.pathname === "/details" ? <img src={ProfileD} alt="imagen original Profile D"/>:
+                        location.pathname === "/forms" ? <img src={ContactUsF} alt="imagen original Contact Us F"/>:
+                        <img src={ProfileC} alt="imagen original Profile C"/>
+                    }
                         <h3>Discover me</h3>
                     </div>
                 </div>
@@ -172,7 +204,58 @@ const AllBoardContent = () => {
                 <RowOfPencils/>
             </div>
         </div>
-        <Modal/>
+        <Modal
+        state= {stateModal1}
+        setState = {SetstateModal1}
+        >
+            {
+                location.pathname === "/landingPages" ?   <h3>holas soy background video</h3>:
+                location.pathname === "/homes" ? <h3>holas soy personal page</h3>:
+                location.pathname === "/details" ? <h3>Holas soy Clothes D</h3>:
+                location.pathname === "/forms" ? <h3>Hola soy Login</h3>:
+                <h3>Holas soy Clothes C</h3>
+            }
+            
+        </Modal>
+        <Modal
+        state= {stateModal2}
+        setState = {SetstateModal2}
+        >
+            {
+                location.pathname === "/landingPages" ?   <h3>holas Minimalism</h3>:
+                location.pathname === "/homes" ? <h3>holas soy E-commerce</h3>:
+                location.pathname === "/details" ? <h3>Holas soy Furniture</h3>:
+                location.pathname === "/forms" ? <h3>Hola soy Register</h3>:
+                <h3>Holas soy Food C</h3>
+            }
+            
+        </Modal>
+        <Modal
+        state= {stateModal3}
+        setState = {SetstateModal3}
+        >
+            {
+                location.pathname === "/landingPages" ?   <h3>holas soy Organic Shapes</h3>:
+                location.pathname === "/homes" ? <h3>holas soy Marketplace</h3>:
+                location.pathname === "/details" ? <h3>Holas News D</h3>:
+                location.pathname === "/forms" ? <h3>Hola soy CheckOut</h3>:
+                <h3>Holas soy News C</h3>
+            }
+            
+        </Modal>
+        <Modal
+        state= {stateModal4}
+        setState = {SetstateModal4}
+        >
+            {
+                location.pathname === "/landingPages" ?   <h3>holas Promotional</h3>:
+                location.pathname === "/homes" ? <h3>holas soy DigitalNewsPaper</h3>:
+                location.pathname === "/details" ? <h3>Holas soy Profile D</h3>:
+                location.pathname === "/forms" ? <h3>Hola soyContact Us</h3>:
+                <h3>Holas soy Profile C</h3>
+            }
+            
+        </Modal>
         </>
     )
 }
--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,24 +1,28 @@
 import './modal.css'
 import { useLocation } from 'react-router-dom';
-const Modal = () => {
+const Modal = ({children, state, setState}) => {
     
     const location = useLocation()
     return(
         <>
-        <div className="overlay-modal">
-            <div className='modal-container'>
-                <div className='modal-header'>
-                    {
-                        location.pathname === '/landingPages' ? <h1 className='lettering-style-L-P'>Landing Pages</h1>:
-                        location.pathname === '/homes' ? <h1>Homes</h1>:
-                        location.pathname === '/details' ? <h1>Details</h1>:
-                        location.pathname === '/forms' ? <h1>Forms</h1>:
-                        <h1>Cards</h1>
-                    }
-                    <button className='close-button'>X</button>
+        {
+            state &&
+                <div className="overlay-modal">
+                    <div className='modal-container'>
+                        <div className='modal-header'>
+                            {
+                                location.pathname === '/landingPages' ? <h1 className='lettering-style-L-P'>Landing Pages</h1>:
+                                location.pathname === '/homes' ? <h1>Homes</h1>:
+                                location.pathname === '/details' ? <h1>Details</h1>:
+                                location.pathname === '/forms' ? <h1>Forms</h1>:
+                                <h1>Cards</h1>
+                            }
+                            <button onClick={() => setState(false)} className='close-button'>X</button>
+                        </div>
+                        {children}
+                    </div>
                 </div>
-            </div>
-        </div>
+        }
         </>
     )
 }
