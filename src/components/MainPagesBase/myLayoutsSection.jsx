72dc598e2fa215d45e7df58c2204e2da652c0654
Paz Zitta <zittapaz@gmail.com>
2022-08-25 19:33:03 -0300

    ready names of layoutSection TWO

--- src/components/MainPagesBase/myLayoutsSection.jsx
+++ src/components/MainPagesBase/myLayoutsSection.jsx
@@ -49,11 +49,25 @@ const MyLayoutsSection = () => {
             </article>
             <article className='order-all-componentes-article-two'>
                 {
-                    location.pathname === '/landingPages'? <h2>MINIMALISM</h2> :
-                    location.pathname === '/homes' ? <h2>E-COMMERCE</h2> :
-                    location.pathname === '/details' ? <h2>FURNITURE</h2> :
-                    location.pathname === '/forms' ? <h2>REGISTER</h2> :
-                    <h2>FOOD</h2>
+                    location.pathname === '/landingPages'?
+                    <div className='name-background-video-article-one'>
+                        <h2>M</h2><h2>I</h2><h2>N</h2><h2>I</h2><h2>M</h2><h2>A</h2><h2>L</h2><h2>I</h2><h2>S</h2><h2>M</h2>
+                    </div>:
+                    location.pathname === '/homes' ? 
+                    <div className='name-background-video-article-one'>
+                        <h2>E</h2><h2>-</h2><h2>C</h2><h2>O</h2><h2>M</h2><h2>M</h2><h2>E</h2><h2>R</h2><h2>C</h2><h2>E</h2>
+                    </div>:
+                    location.pathname === '/details' ? 
+                    <div className='name-background-video-article-one'>
+                        <h2>F</h2><h2>U</h2><h2>R</h2><h2>N</h2><h2>I</h2><h2>T</h2><h2>U</h2><h2>R</h2><h2>E</h2>
+                    </div>:
+                    location.pathname === '/forms' ? 
+                    <div className='name-background-video-article-one'>
+                        <h2>R</h2><h2>E</h2><h2>G</h2><h2>I</h2><h2>S</h2><h2>T</h2><h2>E</h2><h2>R</h2>
+                    </div>:
+                    <div className='name-background-video-article-one'>
+                        <h2>F</h2><h2>O</h2><h2>O</h2><h2>D</h2>
+                    </div>
                 }
                 <div className="top-frame-styles-and-order-layout-section">
                     <div></div>
