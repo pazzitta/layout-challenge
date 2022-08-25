16797663aa2f9a02d26cb8fece3dec7231ab4cd0
Paz Zitta <zittapaz@gmail.com>
2022-08-25 15:56:43 -0300

    finished names of my layoutSection ONE

--- src/components/MainPagesBase/myLayoutsSection.jsx
+++ src/components/MainPagesBase/myLayoutsSection.jsx
@@ -12,11 +12,27 @@ const MyLayoutsSection = () => {
         <div className='general-order-my-layout-section'>
             <article className='order-all-componentes-article-one'>
                 {
-                    location.pathname === '/landingPages'? <h2>BACKGROUND VIDEO</h2> :
-                    location.pathname === '/homes' ? <h2>PERSONAL PAGE</h2> :
-                    location.pathname === '/details' ? <h2>CLOTHES</h2> :
-                    location.pathname === '/forms' ? <h2>LOGIN</h2> :
-                    <h2>CLOTHES</h2>
+                    location.pathname === '/landingPages'? 
+                    <div className='name-background-video-article-one'>
+                        <h2>B</h2><h2>A</h2><h2>C</h2><h2>K</h2><h2>G</h2><h2>R</h2><h2>O</h2><h2>U</h2>
+                        <h2>N</h2><h2>D</h2><h2>V</h2><h2>I</h2><h2>D</h2><h2>E</h2><h2>0</h2>
+                    </div>  :
+                    location.pathname === '/homes' ? 
+                    <div className='name-personal-page-article-one'>
+                        <h2>P</h2><h2>E</h2><h2>R</h2><h2>S</h2><h2>O</h2><h2>N</h2><h2>A</h2><h2>L</h2>
+                        <h2>P</h2><h2>A</h2><h2>G</h2><h2>E</h2>
+                    </div> : 
+                    location.pathname === '/details' ?
+                    <div className='name-clothes-article-one'>
+                        <h2>C</h2><h2>L</h2><h2>O</h2><h2>T</h2><h2>H</h2><h2>E</h2><h2>S</h2>
+                    </div> : 
+                    location.pathname === '/forms' ? 
+                    <div className='name-login-article-one'>
+                        <h2>L</h2><h2>O</h2><h2>G</h2><h2>I</h2><h2>N</h2>
+                    </div> :
+                    <div className='name-clothes-article-one'>
+                        <h2>C</h2><h2>L</h2><h2>O</h2><h2>T</h2><h2>H</h2><h2>E</h2><h2>S</h2>
+                    </div> 
                 }
                 <div className="top-frame-styles-and-order-layout-section">
                     <div></div>
