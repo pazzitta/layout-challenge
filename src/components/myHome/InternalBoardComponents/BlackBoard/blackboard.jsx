d22b08347be66e57aa491c038889fab1a8df3c95
Paz Zitta <zittapaz@gmail.com>
2022-09-29 18:19:37 -0300

    I started with the vertical devices, in these, because I am going to require more media queries than up to now. Complete the allBoardContent component, the wooden poster, the wanted poster, the contact poster, and the @media layout (max-width: 64em) and (orientation: portrait)

--- src/components/myHome/InternalBoardComponents/BlackBoard/blackboard.jsx
+++ src/components/myHome/InternalBoardComponents/BlackBoard/blackboard.jsx
@@ -3,10 +3,11 @@ import './blackBoard.css'
 const Blackboard = () =>{
     return(
         <div className='blackboard-order'>
-            <div></div>
             <div>
-                <div className='blackboard-text-order-and-style'>Hola, soy Paz, Full Stack Developer y este es mi portfolio 
-                    de maquetaciones y estilizado HTML y CSS<p>→</p>
+                <div>
+                    <div className='blackboard-text-order-and-style'>Hola, soy Paz, Full Stack Developer y este es mi portfolio 
+                        de maquetaciones y estilizado HTML y CSS<p>→</p>
+                    </div>
                 </div>
             </div>
         </div>
